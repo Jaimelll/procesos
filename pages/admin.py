9e45f2c310bfee7497b8c2caa69489a9c2d50351
Jaimelll <jaduran0102@gmail.com>
2024-09-25 18:10:24 -0500

    admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Proceso, Evento
 
-# Register your models here.
+@admin.register(Proceso)
+class ProcesoAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'nombre', 'estado', 'fecha_inicio']
+    search_fields = ['numero', 'nombre']
+    list_filter = ['estado', 'direccion', 'grupo']
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ['proceso', 'actividad', 'fecha', 'importe']
+    search_fields = ['proceso__nombre', 'actividad']
+    list_filter = ['fecha']
